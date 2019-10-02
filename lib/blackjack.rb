170904ad9ec7eb3a3610f749c0c357c0b7eeaacc
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-02 16:56:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,13 +61,12 @@ def runner
   welcome 
   card_total = initial_round
   
-  if card_total > 0
+  if card_total 
   until display_card_total(card_total) > 21
   card_total = hit?(card_total)
   display_card_total(card_total)
   end
   end
   end_game(card_total)
-  
 end
     
